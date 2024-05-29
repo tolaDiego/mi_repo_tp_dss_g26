ec5f20caf07b6dca413bc8f13e08166fde5368ba
lisan98 <lisandro0@hotmail.com>
2024-05-29 16:25:46 -0300

    agregue_mesesActiva_y_cambie_esActiva

--- src/main/java/domain/objetos/Heladera.java
+++ src/main/java/domain/objetos/Heladera.java
@@ -9,16 +9,21 @@ import java.util.List;
 public class Heladera {
   private int capacidad;
     private Calendar fechaInicioFuncionamiento;
+    private Calendar fechaFinalServicio;
     private Ubicacion ubicacion;
     private SensorDeMovimiento sensorMovimiento;
     private SensorTemperatura sensorTemperatura;
     private List<Vianda> viandas;
-    public boolean estaActiva() {
-        return true;
+
+    private boolean estadoActivo;
+    public void cambiarEstado (boolean estado) {
+        estadoActivo = estado;
+        if(!estado){ fechaFinalServicio= Calendar.getInstance();}
+
     }
 
     public long mesesActiva() {
-      Calendar fechaFinalServicio= Calendar.getInstance();
+      if(estadoActivo) {fechaFinalServicio= Calendar.getInstance();}
       int anioFinal=fechaFinalServicio.get(Calendar.YEAR);
       int mesFinal=fechaFinalServicio.get(Calendar.MONTH);
       int anioInicial= fechaInicioFuncionamiento.get(Calendar.YEAR);
